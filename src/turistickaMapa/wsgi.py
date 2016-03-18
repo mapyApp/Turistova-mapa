14574df568f99480eedd28d38b7bd11dabc7bbca
root <root@kempelen.ii.fmph.uniba.sk>
2016-03-18 19:19:55 +0100

    well

--- src/turistickaMapa/wsgi.py
+++ src/turistickaMapa/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path = ['/home/turisti/Turistova-mapa/src'] + sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turistickaMapa.settings")
 
 from django.core.wsgi import get_wsgi_application
